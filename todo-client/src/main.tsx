7f355a588d6f986ffe71c5cddfc78889408055c0
mmatsumoto1026 <mmatsumoto1026@gmail.com>
2025-09-01 16:21:24 +0900

    achieve full todo_app functionarity and style one

--- todo-client/src/main.tsx
+++ todo-client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4000" }),
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
