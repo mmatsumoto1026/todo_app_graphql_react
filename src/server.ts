3870bc3f9b6498eeafdc44cb48bb1458e6497589
mmatsumoto1026 <mmatsumoto1026@gmail.com>
2025-09-01 12:11:40 +0900

    create mutations to add, uppdate and delete todos

--- src/server.ts
+++ src/server.ts
@@ -23,11 +23,51 @@ const typeDefs = gql`
   type Query {
     getTodos: [Todo!]!
   }
+
+  type Mutation {
+    addTodo(title: String!): Todo!
+    updateTodo(id: ID!, completed: Boolean!): Todo!
+    deleteTodo(id: ID!): Todo!
+  }
 `;
+
+type AddTodo = {
+  title: string;
+};
 
 const resolvers = {
   Query: {
     getTodos: () => todos,
+  },
+  Mutation: {
+    addTodo: (_: unknown, { title }: AddTodo) => {
+      const newTodo = {
+        id: String(todos.length + 1),
+        title,
+        completed: false,
+      };
+      todos.push(newTodo);
+      return newTodo;
+    },
+    updateTodo: (
+      _: unknown,
+      { id, completed }: { id: string; completed: boolean }
+    ) => {
+      const todo = todos.find((todo) => todo.id === id);
+      if (!todo) {
+        throw new Error("Todo not found");
+      }
+      todo.completed = completed;
+      return todo;
+    },
+    deleteTodo: (_: unknown, { id }: { id: string }) => {
+      const index = todos.findIndex((todo) => todo.id === id);
+      if (index === -1) {
+        throw new Error("Todo not found");
+      }
+      const deletedTodo = todos.splice(index, 1);
+      return deletedTodo[0];
+    },
   },
 };
 
